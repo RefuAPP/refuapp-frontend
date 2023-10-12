393d38ebc9730cead2eff28f5a8c03079f8b69e6
Oriol Agost Batalla <gpg18@agost.info>
2023-10-12 21:08:42 +0200

    feat: add login functionality

--- app/src/app/login/login.page.ts
+++ app/src/app/login/login.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginRequest} from "./schemas/login-request.model";
+import {AuthService} from "../auth/auth.service";
+import {NgForm} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  login: LoginRequest = {
+    username: '',
+    password: '',
+    scope: 'user',
+  }
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm) {
+    if (form.invalid) return;
+
+    this.authService.login(this.login);
+    this.router.navigateByUrl('/home').then(r =>
+      console.log('navigated to home'));
   }
 
 }
