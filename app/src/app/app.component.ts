70954f1e29f06b73cd5eba491bb5b876ff55bcee
FerranAD <ferranaran@tuta.io>
2023-10-19 17:53:52 +0200

    feat: add menu drawer

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public topPages = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Reservations', url: '/reservations', icon: 'folder' },
+  ];
+
+  public bottomPages = [
+    { title: 'Account', url: '/user', icon: 'person' },
+    { title: 'Login', url: '/login', icon: 'log-in' },
+    { title: 'Logout', url: '/logout', icon: 'log-out' },
+  ];
   constructor() {}
 }
