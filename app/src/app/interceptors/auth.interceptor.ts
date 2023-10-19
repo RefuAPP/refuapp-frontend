a21d56d2385fc9338333e6886840c42bc7865e07
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-10-19 12:06:12 +0200

    fix: interceptor auth type, await the promise

--- app/src/app/interceptors/auth.interceptor.ts
+++ app/src/app/interceptors/auth.interceptor.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import {
+  HttpEvent,
+  HttpHandler,
   HttpInterceptor,
   HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpHeaders,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { from, lastValueFrom, Observable } from 'rxjs';
 import { StorageService } from '../services/storage/storage.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private storageService: StorageService) {}
+
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler,
   ): Observable<HttpEvent<any>> {
-    const token = this.storageService.get('token');
+    return from(this.addToken(request, next));
+  }
+
+  private async addToken(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Promise<HttpEvent<any>> {
+    const token = await this.storageService.get('token');
     if (token) {
       request = request.clone({
         setHeaders: {
@@ -24,6 +31,6 @@ export class AuthInterceptor implements HttpInterceptor {
         },
       });
     }
-    return next.handle(request);
+    return await lastValueFrom(next.handle(request));
   }
 }
