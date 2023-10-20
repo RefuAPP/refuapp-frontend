4b39fd2174b336d9137f9a5f8e3e8e3d1680fb3b
FerranAD <ferranaran@tuta.io>
2023-10-20 21:33:54 +0200

    build: set android env for maps api key

--- app/set-env.js
+++ app/set-env.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
 
-const environmentFile = `export const environment = {
+const angularEnvironmentFile = `export const environment = {
   mapsKey: "${process.env['MAPS_API_KEY']}",
 };
 `;
 
-fs.writeFile('./src/environments/environment.secret.ts', environmentFile, err=> {
+const androidEnvironmentFile = `
+MAPS_API_KEY=${process.env['MAPS_API_KEY']}
+`;
+
+fs.writeFile('./src/environments/environment.secret.ts', angularEnvironmentFile, err=> {
   if (err) {
     console.error("Error writing environment.secret.ts file");
     console.error(err);
   } else {
     console.log(`Angular environment.ts file generated`);
+  }
+});
+
+fs.writeFile('./android/local.properties', androidEnvironmentFile, err=> {
+  if (err) {
+    console.error("Error writing local.properties file");
+    console.error(err);
+  } else {
+    console.log(`Android local.properties file generated`);
   }
 });
