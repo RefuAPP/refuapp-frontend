da4c95132edbfe77ef5a04b2fa9fd4ffdf1678ab
alexm3133 <alexandrumateiionut@gmail.com>
2023-10-25 19:07:19 +0200

    Adding profile in app-routing-module

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -59,8 +59,13 @@ const routes: Routes = [
       import('./pages/logout/logout.module').then((m) => m.LogoutPageModule),
   },
   {
+<<<<<<< HEAD
     path: 'reservations',
     loadChildren: () => import('./pages/reservations/reservations.module').then(m => m.ReservationsPageModule)
+=======
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+>>>>>>> 170d51f (Adding profile in app-routing-module)
   },
 ];
 
