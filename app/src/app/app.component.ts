23b1416547160b079c854993c4a1ad120eb1473e
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-11-01 11:22:45 +0100

    feat: add menu language support

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth/auth.service';
-import { map, Observable } from 'rxjs';
+import { combineLatest, map, Observable, Subject } from 'rxjs';
 import { get } from 'scriptjs';
 import { secretEnvironment } from '../environments/environment.secret';
 import { DeviceLanguageService } from './services/translate/device-language.service';
@@ -12,17 +12,6 @@ export interface Page {
   icon: string;
 }
 
-const topPages: Page[] = [
-  { title: 'Home', url: '/home', icon: 'home' },
-  { title: 'Reservations', url: '/reservations', icon: 'folder' },
-];
-
-const bottomPages: Page[] = [
-  { title: 'Account', url: '/profile', icon: 'person' },
-  { title: 'Logout', url: '/logout', icon: 'log-out' },
-  { title: 'Login', url: '/login', icon: 'log-in' },
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -30,29 +19,77 @@ const bottomPages: Page[] = [
 })
 export class AppComponent implements OnInit {
   googleMapsLoaded = false;
+  private topPages: Subject<Page[]> = new Subject<Page[]>();
+  private bottomPages: Subject<Page[]> = new Subject<Page[]>();
+  private topPagesComplete$: Observable<Page[]> = this.topPages.asObservable();
+  private bottomPagesComplete$: Observable<Page[]> =
+    this.bottomPages.asObservable();
+  private isAuthenticated$: Observable<boolean>;
 
-  bottomPages: Observable<Page[]> = this.authService.isAuthenticated().pipe(
-    map((isAuthenticated) => {
-      if (isAuthenticated) return bottomPages.slice(0, 2);
-      else return bottomPages.slice(2, 3);
-    }),
-  );
-
-  topPages: Observable<Page[]> = this.authService.isAuthenticated().pipe(
-    map((isAuthenticated) => {
-      if (isAuthenticated) return topPages;
-      else return topPages.slice(0, 1);
-    }),
-  );
+  bottomPages$: Observable<Page[]>;
+  topPages$: Observable<Page[]>;
 
   constructor(
     private authService: AuthService,
     private translateService: TranslateService,
     private deviceLanguageService: DeviceLanguageService,
   ) {
+    this.isAuthenticated$ = this.authService.isAuthenticated();
+    this.bottomPages$ = combineLatest([
+      this.bottomPagesComplete$,
+      this.isAuthenticated$,
+    ]).pipe(
+      map((result) => {
+        const pages = result[0];
+        const isAuthenticated = result[1];
+        if (isAuthenticated) return pages.slice(0, 2);
+        else return pages.slice(2, 3);
+      }),
+    );
+
+    this.topPages$ = combineLatest([
+      this.topPagesComplete$,
+      this.isAuthenticated$,
+    ]).pipe(
+      map((result) => {
+        const pages = result[0];
+        const isAuthenticated = result[1];
+        if (isAuthenticated) return pages;
+        else return pages.slice(0, 1);
+      }),
+    );
     this.translateService.setDefaultLang('en');
     this.deviceLanguageService.getLanguageCode().subscribe((languageCode) => {
       this.translateService.use(languageCode);
+      this.topPages.next([
+        {
+          title: translateService.instant('MENU.HOME'),
+          url: '/home',
+          icon: 'home',
+        },
+        {
+          title: translateService.instant('MENU.RESERVATIONS'),
+          url: '/reservations',
+          icon: 'folder',
+        },
+      ]);
+      this.bottomPages.next([
+        {
+          title: translateService.instant('MENU.PROFILE'),
+          url: '/profile',
+          icon: 'person',
+        },
+        {
+          title: translateService.instant('MENU.LOGOUT'),
+          url: '/logout',
+          icon: 'log-out',
+        },
+        {
+          title: translateService.instant('MENU.LOGIN'),
+          url: '/login',
+          icon: 'log-in',
+        },
+      ]);
     });
   }
 
