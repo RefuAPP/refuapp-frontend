8be9b488a9399987757fbc8cf52eed65d5be4e24
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-11-01 10:48:48 +0100

    feat: change language from preferences

--- app/src/app/services/translate/device-language.service.ts
+++ app/src/app/services/translate/device-language.service.ts
@@ -2,16 +2,26 @@ import { Injectable } from '@angular/core';
 import { Device } from '@capacitor/device';
 import { map, mergeMap, Observable, timer } from 'rxjs';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
+import { StorageService } from '../storage/storage.service';
+import { TranslateService } from '@ngx-translate/core';
+
+const LANGUAGE_KEY = 'language';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeviceLanguageService {
-  constructor() {}
+  constructor(
+    private storageService: StorageService,
+    private translateService: TranslateService,
+  ) {}
 
   async getCurrentLanguageCode(): Promise<string> {
+    const languageCode = await this.storageService.get(LANGUAGE_KEY);
+    if (languageCode) return languageCode;
     return Device.getLanguageTag().then((languageTag) => languageTag.value);
   }
+
   /**
    * Gets the language code of the device, fetching it every 3 seconds.
    * TODO: This is a workaround for the fact that Capacitor's Device plugin
@@ -20,11 +30,18 @@ export class DeviceLanguageService {
    */
   getLanguageCode(): Observable<string> {
     return timer(0, 3_000).pipe(
-      mergeMap(() => {
-        return fromPromise(Device.getLanguageTag()).pipe(
-          map((languageTag) => languageTag.value),
-        );
-      }),
+      mergeMap(() => fromPromise(this.getCurrentLanguageCode())),
     );
+  }
+
+  async setLanguageCode(languageCode: string): Promise<void | never> {
+    const languageCodes = await this.getLanguagesCodes();
+    if (!languageCodes.includes(languageCode))
+      throw new Error(`Language code ${languageCode} not supported`);
+    await this.storageService.set(LANGUAGE_KEY, languageCode);
+  }
+
+  async getLanguagesCodes(): Promise<string[]> {
+    return this.translateService.getLangs();
   }
 }
