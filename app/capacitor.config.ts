3791490612dc08f1a667c8a18be2d638a7de152a
FerranAD <ferranaran@tuta.io>
2023-11-13 20:16:37 +0100

    refactor: rename android package

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app',
+  appId: 'online.refuapp.app',
+  appName: 'RefuApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
