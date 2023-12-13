979c942949bd170b9b01bcf27b5de10c092c4095
Oriol Agost Batalla <gpg18@agost.info>
2023-12-13 16:07:19 +0100

    refactor: move guards onto `app` package

--- app/src/app/guards/user.guard.ts
+++ app/src/app/guards/user.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthService } from '../app/services/auth/auth.service';
+import { AuthService } from '../services/auth/auth.service';
 
 export const userGuard: CanActivateFn = async () => {
   const authService = inject(AuthService);
